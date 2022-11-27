b6de3b636c6176802c4461766158cd973dba470f
dockerlearn <36786300+dockerlearn@users.noreply.github.com>
2022-11-27 14:24:12 +0530

    Adding yes for installation

--- vagrantinstall.sh
+++ vagrantinstall.sh
@@ -2,8 +2,8 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install virtualbox
+sudo apt install virtualbox -y
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install vagrant
+sudo apt update && sudo apt install vagrant -y
 vagrant up
